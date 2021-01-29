a0e8efcb36a6dab5bb97384eae392d9e19dcfe35
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-29 20:51:33 +0100

    Setup IBOutlets, IBActions

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightSlider.value = 1.5
+        weightSlider.value = 100
+        heightLabel.text = "\(heightSlider.value) m"
+        weightLabel.text = "\(weightSlider.value) kg"
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(String(format: "%.2f", sender.value))
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        //print(String(format: "%.0f", sender.value))
+        print(Int(sender.value))
+    }
 }
 
